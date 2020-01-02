219db2abf1610a32798e42d9ef42db00c5cc7515
minijun <641104000@qq.com>
2020-01-02 16:59:53 +0800

    add test item for upload

--- test/http.js
+++ test/http.js
@@ -13,7 +13,7 @@ describe('#http-test', () => {
     after(() => {//执行完后关闭服务器监听
         server.close();
     });
-    
+
     describe('#test-server', () => {
         it('#test get api/user', async () => {
             await request(server)
@@ -23,19 +23,27 @@ describe('#http-test', () => {
         it('#test post api/login', async () => {
             let res = await request(server)
                 .post('/api/login')
-                .expect(200)
+                .expect(200);
             assert.strictEqual(res.body.status, 400);
             res = await request(server)
                 .post('/api/login')
                 .send({ userName: 'jun2333', password: '123123' })
-                .expect(200)
+                .expect(200);
             // console.log(res.headers);
             assert.strictEqual(res.body.status, 1);
             let cookie = res.headers['set-cookie'];//登录成功后获取cookie用于后面接口测试
+            //测试查询接口
             res = await request(server)
                 .get('/api/user')
-                .set('Cookie', cookie)
+                .set('Cookie', cookie);
             // console.log(res.body);
+            assert.strictEqual(res.body.status, 1);
+            //测试上传接口
+            res = await request(server)
+                .post('/api/upload')
+                .set('Content-Type', 'multipart/form-data')
+                .attach('file', '111.png')
+                .expect(200);
             assert.strictEqual(res.body.status, 1);
         }).timeout(10000);
     })
