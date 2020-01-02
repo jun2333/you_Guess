1f3aab64095e4d97b7f7ec5e5004233fbd74654d
minijun <641104000@qq.com>
2020-01-02 16:19:08 +0800

    delete express-demo

---
+++ egg-example/database/seeders/20191203040049-default-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async(queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    await queryInterface.bulkInsert('users',[
+      {
+        name:'asd',
+        age:14
+      },
+      {
+        name:'qwe',
+        age:14
+      },
+      {
+        name:'ert',
+        age:14
+      }
+    ])
+  },
+
+  down: async(queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   await queryInterface.bulkDelete('users', null, {});
+  }
+};
